0071369d24cb873a573643a095e7f6abc7c239bc
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-08-10 13:06:26 +0300

    added launch on port 3000

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
