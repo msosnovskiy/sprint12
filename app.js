1654d198e6879bce6447d46c0fbb4e7e85edc043
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-08-10 12:59:33 +0300

    added project folder - public

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const app = express();
 
 const {PORT = 3000} = process.env;
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
