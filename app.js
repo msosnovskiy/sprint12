2a5b3485725faa6d2d7ba9fd4cd90cc8b1cac322
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-08-12 15:18:07 +0300

    added error message to unknown address

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const { PORT = 3000 } = process.env;
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/users', users);
 app.use('/cards', cards);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
